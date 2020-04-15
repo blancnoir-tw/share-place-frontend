f2ab9c79f0f23ef55f5e86e837e1f7d84d0406f1
Tatsuya Watanabe <blancnoir.tw@gmail.com>
2020-04-15 09:47:37 +0900

    add basic router setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
+
+import User from './user/pages/User'
+import NewPlace from './places/pages/NewPlace'
 
 const App = () => {
-  return <div>Hello World!!</div>
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <User />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  )
 }
 
 export default App
