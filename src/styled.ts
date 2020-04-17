b5f121a63a7d31c7424eaa6d5f4513862bc8fdf6
Tatsuya Watanabe <blancnoir.tw@gmail.com>
2020-04-17 09:05:42 +0900

    add emotion-theming

---
+++ src/styled.ts
@@ -0,0 +1,42 @@
+import styled, { CreateStyled } from '@emotion/styled'
+
+export const theme = {
+  color: {
+    white: '#fafafa',
+    black: '#333333',
+    primary: {
+      light: '#4f5b62',
+      main: '#263238',
+      dark: '#000a12',
+    },
+    secondary: {
+      light: '#ff5c8d',
+      main: '#d81b60',
+      dark: '#a00037',
+    },
+    error: {
+      light: '#e57373',
+      main: '#f44336',
+      dark: '#d32f2f',
+    },
+    warning: {
+      light: '#ffb74d',
+      main: '#ff9800',
+      dark: '#f57c00',
+    },
+    info: {
+      light: '#2196f3',
+      main: '#64b5f6',
+      dark: '#1976d2',
+    },
+    success: {
+      light: '#81c784',
+      main: '#4caf50',
+      dark: '#388e3c',
+    },
+  },
+}
+
+type Theme = typeof theme
+
+export default styled as CreateStyled<Theme>
